4e8cc068f8d4e3ec751e9250067399527195b658
Anuj Mathur <mathuranuj123@gmail.com>
2020-05-05 01:45:21 +0530

    Database changed to MySQL

--- src/main/java/com/springboot/example/ticketstarter/domain/TestClass.java
+++ src/main/java/com/springboot/example/ticketstarter/domain/TestClass.java
@@ -1,15 +1,23 @@
 package com.springboot.example.ticketstarter.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestClass {
 
 	public static void main(String arg[]) {
-		Runnable i = () -> {
-			int a = 0;
-			for(int l = a;l<=10;l++) {
-				System.out.println(l);
-			}
-		};
-		
-		i.run();
+
+		String rStr = "";
+		String str = "testing";
+		System.out.println(str.length() % 2);
+		System.out.println(str.length() / 2);
+		if (str.length() % 2 == 0) {
+			int charNo = str.length() / 2;
+			rStr = str.substring(charNo - 1, charNo+1);
+		} else {
+			int charNo = str.length() / 2;
+			rStr = str.substring(charNo, charNo +1);
+		}
+		System.out.println(rStr);
 	}
 }
